8316f3d2644b7fa619ad14c98371bed2f836cf75
Brian McClune <brian.mcclune@unnpp.gov>
2022-05-06 14:57:22 +0000

    Integrate bokeh app into modbus client

--- bokeh_modbus/client.py
+++ bokeh_modbus/client.py
@@ -1,13 +1,24 @@
 """A simple asynchronous client."""
 
 import asyncio
+import datetime
 import functools
 import logging
 import time
 
-from typing import List
+from typing import Callable, List, Tuple
 
 import numpy as np
+import tornado.httpserver
+import tornado.ioloop
+
+from bokeh.application import Application
+from bokeh.application.handlers.function import FunctionHandler
+from bokeh.document import Document
+from bokeh.models import ColumnDataSource
+from bokeh.plotting import figure
+from bokeh.server.server import BaseServer
+from bokeh.server.tornado import BokehTornado
 
 from pymodbus.client.asynchronous.async_io import ModbusClientProtocol
 from pymodbus.client.asynchronous.tcp import AsyncModbusTCPClient
@@ -38,40 +49,40 @@ log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
 
 
-def convert_bits(timeb: List[bool], sinb: List[bool]):
+def convert_bits(timeb: List[bool], sinb: List[bool]) -> Tuple[float, float]:
     """Converts `timeb` bits and `sinb` bits to time and float.
 
     Args:
         timeb: 64 bits representing current server time.
         sinb: 32 bits representing current server sin(t).
+
+    Returns:
+        Tuple of `(epoch_time, sin(epoch_time))`.`
     """
 
     # Convert time bits
     bstring = ''.join(['1' if bit else '0' for bit in timeb])
-    localtime = time.localtime(
-        np.asarray(int(bstring, 2), dtype=np.uint64).view(np.float64).item(),
-    )
+    tyme = np.asarray(int(bstring, 2), dtype=np.uint64).view(np.float64).item()
 
     # Convert sin(t) bits
     bstring = ''.join(['1' if bit else '0' for bit in sinb])
     sin = np.asarray(int(bstring, 2), dtype=np.uint32).view(np.float32).item()
 
-    log.debug(
-        'time: %s\tsin(t): %.6f',
-        time.strftime('%H:%M:%S', localtime),
-        sin,
-    )
+    return tyme, sin
 
 
-async def read_coils(protocol: ModbusClientProtocol, interval: float = 0.5):
+async def read_coils(
+    protocol: ModbusClientProtocol,
+) -> Tuple[float, float]:
     """Reads Modbus coils using client `protocol`.
 
     Args:
         protocol: Modbus protocol instance.
-        interval: Interval in seconds between reads.
+
+    Returns:
+        Tuple of `(epoch_time, sin(epoch_time))`.`
     """
 
-    log.debug('Reading coils...')
     while True:
         try:
             # Read coils for time bits
@@ -82,29 +93,69 @@ async def read_coils(protocol: ModbusClientProtocol, interval: float = 0.5):
             rrs = await protocol.read_coils(64, 32)
             sin_bits = rrs.bits
 
-            convert_bits(time_bits, sin_bits)
-            await asyncio.sleep(0.5)
+            epoch_time, sin = convert_bits(time_bits, sin_bits)
+            return epoch_time, sin
         except asyncio.CancelledError:
-            log.debug('Stopped reading coils')
             break
+
+
+def bkapp(doc: Document, protocol: ModbusClientProtocol):
+
+    source = ColumnDataSource(dict(
+        x=[datetime.datetime.fromtimestamp(time.time())],
+        y=[np.sin(time.time())],
+    ))
+
+    p = figure(sizing_mode='stretch_both', x_axis_type='datetime')
+    p.circle(x='x', y='y', source=source)
+    p.x_range.follow = 'end'
+    p.x_range.follow_interval = datetime.timedelta(seconds=15)
+
+    async def callback(
+        # source: ColumnDataSource = source,
+        protocol: ModbusClientProtocol = protocol,
+    ):
+        epoch_time, sin = await read_coils(protocol)
+        x = datetime.datetime.fromtimestamp(epoch_time)
+        y = sin
+        log.debug('time: %s\tsin(t): %.6f', x, y)
+        source.stream(dict(x=[x], y=[y]))
+
+    doc.add_periodic_callback(callback, 200)
+    doc.add_root(p)
 
 
 async def main():
     """Runs the asynchronous client."""
 
-    loop = asyncio.get_running_loop()
-    loop, client = AsyncModbusTCPClient(
+    _, client_task = AsyncModbusTCPClient(
         schedulers.ASYNC_IO,
         port=5020,
-        loop=loop,
+        loop=asyncio.get_running_loop(),
     )
-    client = await client
-    task = loop.create_task(read_coils(client.protocol))
-    await task
+    client = await client_task
+
+    bokeh_app = functools.partial(bkapp, protocol=client.protocol)
+
+    port = 8888
+    tornado_app = BokehTornado(
+        Application(FunctionHandler(bokeh_app)),
+        extra_websocket_origins=[f'localhost:{port}'],
+    )
+    http_server = tornado.httpserver.HTTPServer(tornado_app)
+    http_server.listen(port)
+    bokeh_server = BaseServer(
+        tornado.ioloop.IOLoop.current(),
+        tornado_app,
+        http_server,
+    )
+    bokeh_server.start()
+    await asyncio.Event().wait()
 
 
 if __name__ == '__main__':
     try:
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
         asyncio.run(main())
     except KeyboardInterrupt:
         pass
